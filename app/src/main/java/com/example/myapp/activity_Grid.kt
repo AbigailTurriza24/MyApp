16010558f267634b3667abaa35cd294d28cf8c1f
Abigail Turriza <abigailturriza3@gmail.com>
2022-03-09 01:41:39 -0500

    Commit enlace de pantalla listView con GridView(creacion GridView)

--- app/src/main/java/com/example/myapp/activity_Grid.kt
+++ app/src/main/java/com/example/myapp/activity_Grid.kt
@@ -2,10 +2,32 @@ package com.example.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.ListView
+import android.widget.Toast
 
 class activity_Grid : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grid)
+
+
+
+        var datoss = ArrayList<DatosGrid>()
+        datoss.add(DatosGrid("Text1", R.drawable.cargando2))
+        datoss.add(DatosGrid("Text2", R.drawable.cargando2))
+        datoss.add(DatosGrid("Text3", R.drawable.cargando2))
+        datoss.add(DatosGrid("Text4", R.drawable.cargando2))
+
+        var grid: GridView = findViewById(R.id.grid)
+        //val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frutas)
+        val adaptador = AdaptadorCustomGrid(this, datoss)
+        grid.adapter =adaptador
+
+        grid.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, datoss.get(position).nombre, Toast.LENGTH_LONG).show()
+        }
     }
 }
+
--- app/src/main/java/com/example/myapp/activity_Listview.kt
+++ app/src/main/java/com/example/myapp/activity_Listview.kt
@@ -1,5 +1,6 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AdapterView
@@ -12,7 +13,7 @@ class activity_Listview : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_listview)
-        var datos:ArrayList<DatosListView> = ArrayList()
+        var datos: ArrayList<DatosListView> = ArrayList()
         datos.add(DatosListView("Text1", R.drawable.cargando1))
         datos.add(DatosListView("Text2", R.drawable.cargando1))
         datos.add(DatosListView("Text3", R.drawable.cargando1))
@@ -22,14 +23,22 @@ class activity_Listview : AppCompatActivity() {
 
         val lista = findViewById<ListView>(R.id.lista)
 
-
         //val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frutas)
         val adaptador = AdaptadorCustomList(this, datos)
-
+        lista.isClickable = true
         lista.adapter = adaptador
 
         lista.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
             Toast.makeText(this, datos.get(position).nombre, Toast.LENGTH_LONG).show()
+
+
+            var intent = Intent(this, activity_Grid::class.java)
+            startActivity(intent)
+
+
         }
     }
 }
+
+
+
