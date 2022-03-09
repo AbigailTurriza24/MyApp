9c47b879db350c1c0848dcc601924ccd0977423a
Abigail Turriza <abigailturriza3@gmail.com>
2022-03-08 22:43:10 -0500

    Commit creación de pantalla principal y pantalla de ListView

---
+++ app/src/main/java/com/example/myapp/activity_Listview.kt
@@ -0,0 +1,14 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class activity_Listview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listview)
+    }
+
+
+}
